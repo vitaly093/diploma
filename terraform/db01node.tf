59f34135a6059facf7a78c43ef9b1108a4b9e457
Vitaly Kukhtenko <k.vitaly.93@yandex.ru>
2022-10-21 02:28:12 +0300

    diploma task 4 completed

---
+++ terraform/db01node.tf
@@ -0,0 +1,38 @@
+resource "yandex_compute_instance" "db01-kvitaly-ru" {
+  name                      = "db01-kvitaly-ru"
+  zone                      = "ru-central1-a"
+  hostname                  = "db01-kvitaly-ru"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.ubuntu-2004}"
+      name        = "root-db01-kvitaly-ru"
+      type        = "network-nvme"
+      size        = "50"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.diploma-subnet-1.id}"
+    nat       = true
+    ip_address = "192.168.100.10"
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+output "internal_ip_address_db01-kvitaly-ru" {
+  value = yandex_compute_instance.db01-kvitaly-ru.network_interface.0.ip_address
+}
+
+output "external_ip_address_db01-kvitaly-ru" {
+  value = yandex_compute_instance.db01-kvitaly-ru.network_interface.0.nat_ip_address
+}
